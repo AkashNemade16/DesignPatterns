a78792b468d56d026b7bfc247cfadf13aec713ea
akash-nemade <aksh.akash@gmail.com>
2020-10-05 23:48:17 -0700

    organize

--- Lab3/src/Lab3/GraphTraversor.java
+++
@@ -1,8 +0,0 @@
-package Lab3;
-
-import java.util.List;
-
-interface GraphTraversor {
-       List<Node> DFS_traversing(Node start, Graph g);
-       List<Node> WFS_traversing(Node start, Graph g);
-}
