37123115308d484311412ddc0180d7291c58d909
Akash-Nemade <akash-nemade@pop-os.localdomain>
2020-10-05 23:18:50 -0700

    setup

---
+++ Lab3/src/Lab3/GraphTraversor.java
@@ -0,0 +1,8 @@
+package Lab3;
+
+import java.util.List;
+
+interface GraphTraversor {
+       List<Node> DFS_traversing(Node start, Graph g);
+       List<Node> WFS_traversing(Node start, Graph g);
+}
