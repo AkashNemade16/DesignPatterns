fd984d540a06a294eeda85d21829491ba63cb850
akash-nemade <aksh.akash@gmail.com>
2020-10-06 19:32:38 -0700

    main part

--- Design-Patterns/Lab4/src/lab4/Game.java
+++ Design-Patterns/Lab4/src/lab4/Game.java
@@ -5,5 +5,28 @@ public class Game {
     private Character character;
     private Weapon weapon;
 
+    public void setCharacter( final Character character, final String charactername) {
+        this.character = character;
+        character.AddNewCharacter(charactername);
+    }
+    public void setWeapon( final Weapon weapon,final String weaponName) {
+        this.weapon = weapon;
+        weapon.AddNewWeapon(weaponName);
+    }
 
+    public  Character getCharacter() {
+        return character ;
+    }
+    public Weapon getWeapon() {
+        return weapon ;
+    }
+
+    public void createModes( final Modes mode, final String name, final String weapon) {
+        setCharacter(mode.createCharacter(), name);
+        setWeapon(mode.createWeapons(), weapon);
+    }
+
+    public static void main(String[] args){
+
+    }
 }
