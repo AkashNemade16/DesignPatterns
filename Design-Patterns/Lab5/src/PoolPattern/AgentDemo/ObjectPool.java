d5824d8c13ea84179f0280283edc42c5a49c76c3
akashnemade <aksh.akash@gmail.com>
2020-10-19 10:47:09 -0700

    ObjectPool

--- Design-Patterns/Lab5/src/PoolPattern/AgentDemo/ObjectPool.java
+++ Design-Patterns/Lab5/src/PoolPattern/AgentDemo/ObjectPool.java
@@ -2,41 +2,55 @@ package PoolPattern.AgentDemo;
 
 public class ObjectPool implements ObjectPool_IF {
 
-    private Object lockObject;
+    private static ObjectPool poolInstance;
+    private final Object lockObject;
     private int size;
     private int maxInstances;
+    private int instanceCount;
+    private ObjectCreation_IF c;
     private Object[] pool;
+    public Object server;
 
-    private ObjectPool(ObjectCreation_IF c, int max){
-
+    private ObjectPool(ObjectCreation_IF c, int max) {
+        this.c = c;
+        this.size = 0;
+        this.instanceCount = 0;
+        this.maxInstances = max;
+        this.lockObject = new Object();
+        pool = new Object[maxInstances];
     }
-    @Override
+
+    public synchronized static ObjectPool getPoolInstance(ObjectCreation_IF c, int max) {
+        if (poolInstance == null)
+            poolInstance = new ObjectPool(c, max);
+        return poolInstance;
+    }
+
     public int getSize() {
-        return 0;
+        return this.size;
     }
 
-    @Override
     public int getCapacity() {
-        return 0;
+        return pool.length;
     }
 
-    @Override
+    public int getInstanceCount() {
+        return this.instanceCount;
+    }
+
+    public int getMaxInstances() {
+        return maxInstances;
+    }
+
     public void setCapacity(int c) {
-
-    }
-
-    @Override
-    public Object getObject() {
-        return null;
-    }
-
-    @Override
-    public Object waitForObject() {
-        return null;
-    }
-
-    @Override
-    public void release(Object o) {
-
+        if (c <= 0) {
+            String msg = "Capacity must be greater than zero";
+            throw new IllegalArgumentException(msg);
+        }
+        synchronized (lockObject) {
+            Object[] newPool = new Object[c];
+            System.arraycopy(pool, 0, newPool, 0, c);
+            pool = newPool;
+        }
     }
 }
